5868132a3f0f7c1dbc00934aeaf3561dd50f9df7
gginglis <inglisge23@craighead.school.nz>
2023-06-01 10:32:14 +1200

    made the life remaining calculator

--- code/4-test-life-remaining.js
+++ code/4-test-life-remaining.js
@@ -3,5 +3,11 @@
 // ####################################
 
 function lifeRemaining() {
-  
+    let age = prompt('How old are you?')
+
+    let yearsLeft = (83.5 - age)
+
+    alert('You have ' + yearsLeft + ' years left in you')
+
+    alert('skill issue')
 }
