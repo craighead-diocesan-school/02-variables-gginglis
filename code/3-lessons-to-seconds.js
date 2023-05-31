199429209ca4f7f479dc307db2be94aa0b9ed551
gginglis <inglisge23@craighead.school.nz>
2023-06-01 10:27:01 +1200

    made the seconds left calculator

--- code/3-lessons-to-seconds.js
+++ code/3-lessons-to-seconds.js
@@ -4,4 +4,10 @@
 
 function lessonsToSeconds() {
   alert('There are 60 minutes in a lesson, and 60 seconds in a minute.')
+
+  let lessons = prompt('How many lessons do you have left today?')
+
+  let secondsLeft = (lessons * 3600)
+
+  alert("That's " + secondsLeft + " seconds ☹️")
 }
