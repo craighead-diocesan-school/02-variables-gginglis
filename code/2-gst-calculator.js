49ca474789388b5842b56b507cd9d4fccee01c1e
gginglis <inglisge23@craighead.school.nz>
2023-05-31 19:26:48 +1200

    made the gst calculator

--- code/2-gst-calculator.js
+++ code/2-gst-calculator.js
@@ -4,4 +4,10 @@
 
 function gstCalculator() {
   alert('The price with GST the price and 15%.')
+
+  let cost = prompt('What is the cost before GST ?')
+
+  let gstPrice = (cost * 1.15)
+
+  alert('The cost with GST is $' + gstPrice)
 }
